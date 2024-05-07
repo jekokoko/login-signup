26aaeb9e50f1cfa3d48b33ada9474cf904c9b4d4
jeko <jerichomacarang21@gmail.com>
2024-05-07 12:04:55 +0800

    admin

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginSignup from './Components/LoginSignup/LoginSignup';
+import user_icon from '../src/Components/Assets/person.png'
+import email_icon from '../src/Components/Assets/email.png'
+import password_icon from '../src/Components/Assets/password.png'
+
+
+
+function App() {
+  return (
+    <div>
+      <LoginSignup/>
+    </div>
+  );
+}
+
+export default App;
